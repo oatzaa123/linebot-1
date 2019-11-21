220ff99e8b570d7cf8bd44ae448f91791190d062
oatzaa123 <53205207+oatzaa123@users.noreply.github.com>
2019-11-21 16:40:06 +0700

    Update bot-flex.php

--- bot-flex.php
+++ bot-flex.php
@@ -6,7 +6,7 @@ $ACCESS_TOKEN = 'viahgF0U1r8cCxs6JmmQeClrBHE3Sng0L2eDKKGhIV5vv/i+Vl+Up9eooWqor+A
 $channelSecret = '8efdfa82f3fa7f1e0ee974a83b5c6e05';
 
 
-$POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer' , $ACCESS_TOKEN);
+$POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer viahgF0U1r8cCxs6JmmQeClrBHE3Sng0L2eDKKGhIV5vv/i+Vl+Up9eooWqor+ABRue6wumafsXliuy7VRjo9trjdcPmFLcLggrp3A2SOBm4L1asIjUTVtJaiR9dCcL6pkF6nsZQBJRI90N6aZdXigdB04t89/1O/w1cDnyilFU=);
 
 $request = file_get_contents('php://input');   // Get request content
 $request_array = json_decode($request, true);   // Decode JSON to Array
