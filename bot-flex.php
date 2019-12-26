805a611384a0a9f156537f33021d7725709f572d
oatzaa123 <53205207+oatzaa123@users.noreply.github.com>
2019-12-26 14:32:01 +0700

    Update bot-flex.php

--- bot-flex.php
+++ bot-flex.php
@@ -1,8 +1,5 @@
 <?php
 
-require 'vendor/autoload.php';
-use Google\Cloud\Firestore\FirestoreClient;
-
 $API_URL = 'https://api.line.me/v2/bot/message';
 $ACCESS_TOKEN = 'viahgF0U1r8cCxs6JmmQeClrBHE3Sng0L2eDKKGhIV5vv/i+Vl+Up9eooWqor+ABRue6wumafsXliuy7VRjo9trjdcPmFLcLggrp3A2SOBm4L1asIjUTVtJaiR9dCcL6pkF6nsZQBJRI90N6aZdXigdB04t89/1O/w1cDnyilFU='; 
 $channelSecret = '8efdfa82f3fa7f1e0ee974a83b5c6e05';
@@ -87,14 +84,6 @@ $jsonFlex = [
   ],
 ];
 
-$firestore = new FirestoreClient();
-
-$collectionReference = $firestore->collection('Flex message');
-$documentReference = $collectionReference->document($Text);
-$snapshot = $documentReference->snapshot();
-
-echo "Hello " . $snapshot['Text'];
-
 
 if ( sizeof($request_array['events']) > 0 ) {
     foreach ($request_array['events'] as $event) {
@@ -105,7 +94,7 @@ if ( sizeof($request_array['events']) > 0 ) {
 
         $data = [
             'replyToken' => $reply_token,
-            'messages' => [$collectionReference]
+            'messages' => [$jsonFlex]
         ];
 
         print_r($data);
