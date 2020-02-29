93c4df0bc1279bb089f65ff53d7443aa27c1a2ef
oatzaa123 <53205207+oatzaa123@users.noreply.github.com>
2020-02-29 22:26:31 +0700

    Update database_connection.php

--- database_connection.php
+++ database_connection.php
@@ -1,3 +1,3 @@
 <?php
-$connect = new PDO("mysql:host=c81f01f0.ngrok.io;dbname=flexmessage", "root", "");
+$connect = new PDO("mysql:host=us-cdbr-iron-east-04.cleardb.net;dbname=heroku_78e9a719b1cc08e", "b6067353944e3b", "d0750afa");
 ?>
