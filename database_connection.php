56100d717e96c1f027e2e9667982211a5c4d6aa6
oatzaa123 <53205207+oatzaa123@users.noreply.github.com>
2020-02-29 22:20:29 +0700

    Update database_connection.php

--- database_connection.php
+++ database_connection.php
@@ -1,3 +1,3 @@
 <?php
-$connect = new PDO("mysql:host=us-cdbr-iron-east-04.cleardb.net;dbname=heroku_78e9a719b1cc08e", "b6067353944e3b", "d0750afa");
+$connect = new PDO("mysql:host=localhost;dbname=flexmessage", "root", "");
 ?>
