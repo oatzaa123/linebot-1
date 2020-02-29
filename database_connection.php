bc09d56e4eaf45804f38d15d4eb2f29ceb9befb1
oatzaa123 <53205207+oatzaa123@users.noreply.github.com>
2020-02-29 21:52:24 +0700

    Update database_connection.php

--- database_connection.php
+++ database_connection.php
@@ -1,7 +1,3 @@
 <?php
-$host = "us-cdbr-iron-east-04.cleardb.net";
-$username = "b6067353944e3b";
-$password = "d0750afa";
-$connect = mysqli_connect($host,$username,$password);
-$db = mysqli_select_db("heroku_78e9a719b1cc08e")
+$connect = new PDO("mysql:host=us-cdbr-iron-east-04.cleardb.net;dbname=heroku_78e9a719b1cc08e", "b6067353944e3b", "d0750afa");
 ?>
