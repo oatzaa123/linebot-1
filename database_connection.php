87336c6b9c0a4137e9a5c5b4c28d5e0982e319a8
oatzaa123 <53205207+oatzaa123@users.noreply.github.com>
2020-02-29 21:21:01 +0700

    Update database_connection.php

--- database_connection.php
+++ database_connection.php
@@ -1,7 +1,7 @@
 <?php
-$host = "localhost";
-$username = "root";
-$password = "";
-$dbname = "flexmessage";
-$connect = mysqli_connect($host,$username,$password,$dbname);
+$host = "us-cdbr-iron-east-04.cleardb.net";
+$username = "b6067353944e3b";
+$password = "d0750afa";
+$connect = mysqli_connect($host,$username,$password);
+$db = mysqli_select_db("heroku_78e9a719b1cc08e")
 ?>
