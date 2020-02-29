aa64225ec9222604496695167202467f68558f64
oatzaa123 <53205207+oatzaa123@users.noreply.github.com>
2020-02-29 21:07:17 +0700

    Update database_connection.php

--- database_connection.php
+++ database_connection.php
@@ -3,5 +3,5 @@ $host = "localhost";
 $username = "root";
 $password = "";
 $dbname = "flexmessage";
-mysqli_connect($host,$username,$password,$dbname);
+$connect = mysqli_connect($host,$username,$password,$dbname);
 ?>
