d85335edd9051b9828dcbd10e6c1675b5bf7e290
oatzaa123 <53205207+oatzaa123@users.noreply.github.com>
2020-03-17 13:36:03 +0700

    Update beacon.php

--- beacon.php
+++ beacon.php
@@ -4,9 +4,13 @@ include("database_connection.php");
 $API_URL = 'https://api.line.me/v2/bot/message';
 $ACCESS_TOKEN = 'viahgF0U1r8cCxs6JmmQeClrBHE3Sng0L2eDKKGhIV5vv/i+Vl+Up9eooWqor+ABRue6wumafsXliuy7VRjo9trjdcPmFLcLggrp3A2SOBm4L1asIjUTVtJaiR9dCcL6pkF6nsZQBJRI90N6aZdXigdB04t89/1O/w1cDnyilFU='; 
 $channelSecret = '8efdfa82f3fa7f1e0ee974a83b5c6e05';
+
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
+
 $request = file_get_contents('php://input');   // Get request content
 $request_array = json_decode($request, true);   // Decode JSON to Array
+var_export($request_array);
+
 $jsonFlex = '';
 $query = "SELECT * FROM product";
 $statement = $connect->prepare($query);
@@ -90,7 +94,8 @@ $result = $statement->fetchAll();
  ] 
 ];
 }
-if (isset($request_array['events'])) > 0 {
+
+if (isset($request_array['events']) > 0) {
     foreach ($request_array['events'] as $event) {
         error_log(json_encode($event));
         $reply_message = '';
