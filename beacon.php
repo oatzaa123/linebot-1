0cbd06e8b299362e44ccdc6677565183073dd8eb
oatzaa123 <53205207+oatzaa123@users.noreply.github.com>
2020-03-19 15:49:35 +0700

    Update beacon.php

--- beacon.php
+++ beacon.php
@@ -12,7 +12,7 @@ $request_array = json_decode($request, true);   // Decode JSON to Array
 var_export($request_array);
 $jsonFlex = [];
 
-$query = "SELECT * FROM product";
+$query = "SELECT * FROM product WHERE id = 12";
 $statement = $connect->prepare($query);
 $statement->execute();
 $result = $statement->fetchAll();
