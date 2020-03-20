f23a179fd60b2d90bceb1c639d9873d45a43afe1
oatzaa123 <53205207+oatzaa123@users.noreply.github.com>
2020-03-20 14:01:49 +0700

    Update beacon.php

--- beacon.php
+++ beacon.php
@@ -89,9 +89,61 @@ $jsonFlex = [
                                           ] 
                            ] 
                         ] 
-                  ] 
+                  ], 
+            [
+                                                      "type" => "bubble", 
+                                                      "direction" => "ltr", 
+                                                      "hero" => [
+                                                         "type" => "image", 
+                                                         "url" => "https://websbackend.herokuapp.com/uploads/".$row['product_img'], 
+                                                         "size" => "full", 
+                                                         "aspectRatio" => "20:13", 
+                                                         "aspectMode" => "cover" 
+                                                      ], 
+                                                      "body" => [
+                                                            "type" => "box", 
+                                                            "layout" => "vertical", 
+                                                            "spacing" => "sm", 
+                                                            "contents" => [
+                                                               [
+                                                                  "type" => "text", 
+                                                                  "text" => $row['product_name'], 
+                                                                  "size" => "xl", 
+                                                                  "align" => "center", 
+                                                                  "gravity" => "center", 
+                                                                  "weight" => "bold", 
+                                                                  "wrap" => true 
+                                                               ], 
+                                                               [
+                                                                     "type" => "box", 
+                                                                     "layout" => "baseline", 
+                                                                     "contents" => [
+                                                                        [
+                                                                           "type" => "text", 
+                                                                           "text" => $row['product_price'], 
+                                                                           "size" => "xl", 
+                                                                           "align" => "center", 
+                                                                           "weight" => "bold", 
+                                                                           "wrap" => true 
+                                                                        ] 
+                                                                     ] 
+                                                                  ], 
+                                                               [
+                                                                              "type" => "box", 
+                                                                              "layout" => "vertical", 
+                                                                              "contents" => [
+                                                                                 [
+                                                                                    "type" => "text", 
+                                                                                    "text" => $row['product_details'], 
+                                                                                    "align" => "center" 
+                                                                                 ] 
+                                                                              ] 
+                                                                           ] 
+                                                            ] 
+                                                         ] 
+                                                   ] 
          ] 
-      ] 
+      ]   
 ]; 
 }
 }
